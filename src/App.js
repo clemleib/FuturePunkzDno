fca01e1310eb340642345e83ad7d4b1909615db4
clemleib <remainsane0@gmail.com>
2022-01-11 08:31:17 -0600

    ch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.css";
 import Home from "./pages/Home";
 import Mint from "./pages/Mint";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
